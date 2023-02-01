f19326cdbfa84896953c23b1850e6cc796ba9364
Tatsuya Sasaki <sasaki@crystalcode.jp>
2023-02-01 18:21:47 +0900

    fix: format

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,25 @@
-import Image from 'next/image'
-import {Swiper, SwiperSlide} from 'swiper/react';
-import 'swiper/css';
+import Image from "next/image";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
 
 export default function Home() {
-    return (
-        <div className="max-w-screen-md mx-auto">
-            <h1 className="m-4">Carousel</h1>
-            <Swiper>
-                <SwiperSlide>
-                    <Image src="/slide1.png" alt="slide1" width={1600} height={900}/>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image src="/slide2.png" alt="slide2" width={1600} height={900}/>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image src="/slide3.png" alt="slide3" width={1600} height={900}/>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <Image src="/slide4.png" alt="slide4" width={1600} height={900}/>
-                </SwiperSlide>
-            </Swiper>
-        </div>
-    );
-};
+  return (
+    <div className="max-w-screen-md mx-auto">
+      <h1 className="m-4">Carousel</h1>
+      <Swiper>
+        <SwiperSlide>
+          <Image src="/slide1.png" alt="slide1" width={1600} height={900} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src="/slide2.png" alt="slide2" width={1600} height={900} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src="/slide3.png" alt="slide3" width={1600} height={900} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <Image src="/slide4.png" alt="slide4" width={1600} height={900} />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
